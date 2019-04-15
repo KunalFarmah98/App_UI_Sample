51667e4c67c5ab69623db0828cf65106cf3a7d01
KUNAL FARMAH <kunalfarmah98@gmail.com>
2019-04-15 13:39:46 +0530

    Added list

---
+++ app/src/main/java/com/apps/kunalfarmah/connect_my_match_assign/Study.java
@@ -0,0 +1,4 @@
+package com.apps.kunalfarmah.connect_my_match_assign;
+
+public class Study {
+}
---
+++ app/src/main/java/com/apps/kunalfarmah/connect_my_match_assign/StudyAdapter.java
@@ -0,0 +1,6 @@
+package com.apps.kunalfarmah.connect_my_match_assign;
+
+import android.support.v7.widget.RecyclerView;
+
+public class StudyAdapter extends RecyclerView.Adapter {
+}
